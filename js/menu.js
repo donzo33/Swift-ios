e55b74fcba7a9013397738b6bb7343f6f7e48361
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-11 17:42:04 +0200

    Avance bouton menu

--- js/menu.js
+++ js/menu.js
@@ -9,12 +9,18 @@ $(function() {
          if (this.isMenuOpen) { // On utilise this pour accéder aux propriétés de menu lui meme
             $("#menu-content").removeClass(this.cssStates.opened);
             $("#menu-content").addClass(this.cssStates.closed); // On enléve l'ancienne classe et ajoute la nouvelle
-
-            $("#menu-button-top").removeClass("openButton");
-            $("#menu-button-top").addClass("closeButton");
          } else {
             $("#menu-content").removeClass(this.cssStates.closed);
             $("#menu-content").addClass(this.cssStates.opened);
+
+            $("#menu-button-top").removeClass("openButton");
+            $("#menu-button-top").addClass("closeButton");
+
+            $("#menu-button-bottom").removeClass("openButton");
+            $("#menu-button-bottom").addClass("closeButton");
+
+            $("#menu-button-middle").removeClass("openButton");
+            $("#menu-button-middle").addClass("closeButton");
          }
          this.isMenuOpen = !this.isMenuOpen; // On inverse le boolean
       }
