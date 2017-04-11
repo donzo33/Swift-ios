13f413f2749a7bfd4e5260a08cd7a2aa5384281e
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-11 13:55:03 +0200

    Début de menu, il se deplie au clic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function() {
-	
+   var menu = {
+      isMenuOpen: false,
+      cssStates: {
+         opened: "menu-opened",
+         closed: "menu-closed"
+      },
+      toggleState: function() { // Fonction pour afficher/cacher le menu hamburger
+         if (this.isMenuOpen) { // On utilise this pour accéder aux propriétés de menu lui meme
+            $("#menu").removeClass(this.cssStates.opened);
+            $("#menu").addClass(this.cssStates.closed); // On enléve l'ancienne classe et ajoute la nouvelle
+         } else {
+            $("#menu").removeClass(this.cssStates.closed);
+            $("#menu").addClass(this.cssStates.opened);
+         }
+         this.isMenuOpen = !this.isMenuOpen; // On inverse le boolean
+      }
+   };
+
+   $("#menu").click(function() {
+      menu.toggleState();
+   });
 })
