eda6fa738b67f2fb76d3049725adbbf0104538ae
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-19 15:07:48 +0200

    Animation et JS lors du clic sur reponse

--- js/epreuve.js
+++ js/epreuve.js
@@ -42,17 +42,18 @@ $(function() {
 
       // On parcours le tableau des réponses et on les ajoute (data-answer est utilisé pour retrouver la bonne réponse)
       for (let iA = 0; iA < a.length; iA++) {
-         $(".question-answers", path).append("<div data-id='" + i + "' data-answer='" + a[iA].answer + "' class='question-answer col-xs-4'>" + a[iA].text + "</div>");
+         $(".question-answers", path).append("<div id='q" + i + "a" + iA + "' data-question-id='" + i + "' data-answer='" + a[iA].answer +
+                                             "' class='question-answer animated col-xs-4'>" + a[iA].text + "</div>");
       }
    }
 
    $(".question-answer").click(function(origin) {
-      if (! (typeof origin.target.dataset.answer === "string" && typeof origin.target.dataset.id === "string")) {
+      if (! (typeof origin.target.dataset.answer === "string" && typeof origin.target.dataset.questionId === "string")) {
          return null; // Protection
       }
 
       var isGoodAnswer = (origin.target.dataset.answer == 'true'); // On utilise le dataset pour retrouver l'id et si c une bonne réponse
-      var id = parseInt(origin.target.dataset.id);
+      var id = parseInt(origin.target.dataset.questionId);
 
       if (! (typeof isGoodAnswer === "boolean" && typeof id === "number")) {
          return null; // Protection
@@ -61,12 +62,21 @@ $(function() {
       // On peut débuter le traitement
 
       studentResult[id] = isGoodAnswer; // On ajoute sa réponse a ses résultats
-      console.log(studentResult);
       
+      var answer = $("#" + origin.target.id);
+      answer.addClass("flipOutX"); // On lance l'anim
+
+      answer.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', // On lui ajoute un callback de fin d'anim
+         function(e) {
+            answer.removeClass("flipOutX");
+            answer.addClass("flipInX");
+         }
+      );
+
       if (isGoodAnswer) {
-
+         answer.addClass("question-right");
       } else {
-
+         answer.addClass("question-wrong");
       }
    })
 })
