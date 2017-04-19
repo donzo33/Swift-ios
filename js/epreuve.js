b9a7a083e978c2a9539a67f9801835b115480f43
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-19 16:36:40 +0200

    Questionnaire quasi fonctionnel

--- js/epreuve.js
+++ js/epreuve.js
@@ -23,7 +23,7 @@ var studentResult = [];
 $(function() {
    for (let i = 0; i < questions.length; i++) {
       var path = "#questionnaire-questions-row", q = questions[i], a = q.answers;
-      studentResult.push(false); // On initie les résultats de l'éléve a false
+      studentResult.push("unanswered"); // On initie les résultats de l'éléve a false
       
       jQuery('<div/>', { // On crée le div de la question
          id: 'question-n-' + i,
@@ -43,7 +43,7 @@ $(function() {
       // On parcours le tableau des réponses et on les ajoute (data-answer est utilisé pour retrouver la bonne réponse)
       for (let iA = 0; iA < a.length; iA++) {
          $(".question-answers", path).append("<div id='q" + i + "a" + iA + "' data-question-id='" + i + "' data-answer='" + a[iA].answer +
-                                             "' class='question-answer animated col-xs-4'>" + a[iA].text + "</div>");
+                                             "' class='question-answer question-answer-alive animated col-xs-4'>" + a[iA].text + "</div>");
       }
    }
 
@@ -57,26 +57,40 @@ $(function() {
 
       if (! (typeof isGoodAnswer === "boolean" && typeof id === "number")) {
          return null; // Protection
+      } else if (studentResult[id] != "unanswered") { // else if plutot que ou par lisibilité
+         return null; // Si la question a déja été répondue alors on ne fait rien
       }
 
       // On peut débuter le traitement
-
-      studentResult[id] = isGoodAnswer; // On ajoute sa réponse a ses résultats
+      studentResult[id] = isGoodAnswer ? "right" : "wrong"; // On ajoute sa réponse a ses résultats
+      console.log(studentResult);
       
       var answer = $("#" + origin.target.id);
       answer.addClass("flipOutX"); // On lance l'anim
-
       answer.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', // On lui ajoute un callback de fin d'anim
          function(e) {
-            answer.removeClass("flipOutX");
-            answer.addClass("flipInX");
+            if (answer.hasClass("flipOutX")) {
+               answer.removeClass("flipOutX");
+               answer.addClass("flipInX");
+            } else {
+               answer.removeClass("flipInX");
+               isGoodAnswer ? answer.addClass("tada") : answer.addClass("jello");
+            }
          }
       );
 
+      // On enléve la classe 'alive' pour désactiver le hover
+      var qAnswers = origin.target.offsetParent.childNodes[1].childNodes; // Wtf?
+      for (let i = 0; i < qAnswers.length; i++) { // On parcours le tableau des réponses de la question
+         var node = $("#" + qAnswers[i].id);
+         node.removeClass("question-answer-alive");
+         setTimeout(() => $("#" + qAnswers[i].id).addClass("question-disabled"), 500);
+      }
+
       if (isGoodAnswer) {
-         answer.addClass("question-right");
+         setTimeout(() => answer.addClass("question-right"), 500);
       } else {
-         answer.addClass("question-wrong");
+         setTimeout(() => answer.addClass("question-wrong"), 500);
       }
    })
 })
