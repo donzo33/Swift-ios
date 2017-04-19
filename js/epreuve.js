18d81e3b5cac34e39131fa34ea58f409435bf435
Wiskyt <samy.bouamama@hotmail.fr>
2017-04-19 11:34:53 +0200

    Improve epreuve.js

--- js/epreuve.js
+++ js/epreuve.js
@@ -18,9 +18,12 @@ var questions = [
    },
 ];
 
+var studentResult = [];
+
 $(function() {
    for (let i = 0; i < questions.length; i++) {
       var path = "#questionnaire-questions-row", q = questions[i], a = q.answers;
+      studentResult.push(false); // On initie les résultats de l'éléve a false
       
       jQuery('<div/>', { // On crée le div de la question
          id: 'question-n-' + i,
@@ -39,11 +42,31 @@ $(function() {
 
       // On parcours le tableau des réponses et on les ajoute (data-answer est utilisé pour retrouver la bonne réponse)
       for (let iA = 0; iA < a.length; iA++) {
-         $(".question-answers", path).append("<div data-answer='" + a[iA].answer + "' class='question-answer col-xs-4'>" + a[iA].text + "</div>");
+         $(".question-answers", path).append("<div data-id='" + i + "' data-answer='" + a[iA].answer + "' class='question-answer col-xs-4'>" + a[iA].text + "</div>");
       }
    }
 
    $(".question-answer").click(function(origin) {
-      var isGoodAnswer = origin.target.dataset.answer; // On utilise le dataset pour retrouver
+      if (! (typeof origin.target.dataset.answer === "string" && typeof origin.target.dataset.id === "string")) {
+         return null; // Protection
+      }
+
+      var isGoodAnswer = (origin.target.dataset.answer == 'true'); // On utilise le dataset pour retrouver l'id et si c une bonne réponse
+      var id = parseInt(origin.target.dataset.id);
+
+      if (! (typeof isGoodAnswer === "boolean" && typeof id === "number")) {
+         return null; // Protection
+      }
+
+      // On peut débuter le traitement
+
+      studentResult[id] = isGoodAnswer; // On ajoute sa réponse a ses résultats
+      console.log(studentResult);
+      
+      if (isGoodAnswer) {
+
+      } else {
+
+      }
    })
 })
